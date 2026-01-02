03b6e6b7ccaba1dbdb70999ed9824bda9265d7bc
krish <krishstha202@gmail.com>
2026-01-02 15:01:15 +0545

    modified

--- lib/features/auth/presentation/pages/signup_page.dart
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -166,7 +166,6 @@ class _SignupScreenState extends ConsumerState<SignupScreen> {
 
             const SizedBox(height: 30),
 
-            // Form card
             Container(
               width: double.infinity,
               margin: const EdgeInsets.symmetric(horizontal: 20),
